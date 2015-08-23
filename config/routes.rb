3439c026fcc15df53a2ff66bcafeb1632910651c
Akshat Adawal <akshat@nestaway.com>
2015-08-23 13:47:30 +0530

    Adding user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: :json },
   constraints: { subdomain: 'api' }, path: '/' do
     #List our resources here
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    end
   end
 end
